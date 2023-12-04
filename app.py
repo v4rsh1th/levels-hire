fe69c2656f4b19e6b0c44c8e3b125cfb9558a0db
v4rsh1th <v4rsh1th@gmail.com>
2023-12-03 21:54:34 -0600

    big fixed: process_skills

--- app.py
+++ app.py
@@ -37,6 +37,7 @@ def process_text():
     if not exact_match.empty:
         skills_list = exact_match['Skills Required'].values[0]
         result = f"Exact match found!\nJob Role: {exact_match['Job Role Title'].values[0]}\nSkills: {skills_list}"
+
     else:
         user_input_vector = tfidf_vectorizer.transform([user_input])
         similarities = cosine_similarity(user_input_vector, tfidf_matrix)
@@ -45,10 +46,10 @@ def process_text():
         similar_roles = dataset[similarities[0] > threshold]
 
         if not similar_roles.empty:
-            result = "Similar job roles found:\n"
+            result = "Similar job roles found:\n\n"
             for _, row in similar_roles.head(3).iterrows():
                 skills_list = row['Skills Required']
-                result += f"Job Role: {row['Job Role Title']}\nSkills: {skills_list} \n"
+                result += f"Job Role: {row['Job Role Title']}\nSkills: {skills_list} \n\n"
         else:
             result = "No matching job roles found."
     return render_template('index.html', result=result)
@@ -56,26 +57,26 @@ def process_text():
 @app.route('/process_skills', methods=['POST'])
 def process_skills():
     user_skills = request.form['user_skills']
-    # exact_match = dataset[dataset['Job Role Title'].str.lower() == user_input.lower()]
-    # Check for an exact match
     exact_match = dataset[dataset['Job Role Title'].str.lower() == user_skills.lower()]
 
     if not exact_match.empty:
         suitable_job = f"Exact match found!\nJob Role: {exact_match.iloc[0]['Job Role Title']}"
     else:
-        # If no exact match, find the best match based on word similarity
         user_input_vector = vectorizer.transform([user_skills])
 
         similarities = cosine_similarity(user_input_vector, skills_matrix)
 
-        best_match_index = similarities.argmax()
-        best_match = dataset.iloc[best_match_index]['Job Role Title']
+        # Define a threshold to filter out low similarity scores
+        threshold = 0.2
+        matching_indices = similarities[0] > threshold
 
-        if best_match:
-            suitable_job = f"Best match found!\nJob Role: {best_match}"
+        if any(matching_indices):
+            best_match_index = similarities.argmax()
+            best_match = dataset.iloc[best_match_index]['Job Role Title']
+            suitable_job = f"Best match found! \nJob Role: {best_match}"
         else:
             suitable_job = "No matching job roles found."
-
+            
     return render_template('index.html', suitable_job=suitable_job)
 
 if __name__ == '__main__':
